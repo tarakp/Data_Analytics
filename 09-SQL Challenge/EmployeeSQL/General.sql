9cf382687234a49910f9090c849301e8e39db8b2
tarakP <tarakp@hotmail.com>
2021-05-06 17:50:10 -0700

    09-SQL-First Commit

---
+++ 09-SQL Challenge/EmployeeSQL/General.sql
@@ -0,0 +1,15 @@
+-----Successfully imported.
+select * from salaries;
+select * from employees;
+select * from title;
+
+----Not imported yet
+select * from department_manager;
+select * from departments;
+select * from department_employee;
+
+
+ CONSTRAINT "pk_employee_department" PRIMARY KEY (
+        "emp_no"
+     )
+
---
+++ 09-SQL Challenge/EmployeeSQL/SQL - Create Table.sql
@@ -0,0 +1,63 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/T0kOSe
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS department_employee;
+DROP TABLE IF EXISTS department_manager;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS title;
+
+CREATE TABLE "departments" (
+    "dept_no" varchar(10)   NOT NULL,
+    "dept_name" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "department_employee" (
+    "emp_no" int   NOT NULL,
+    "dept_no" varchar(10)   NOT NULL
+   
+);
+
+CREATE TABLE "department_manager" (
+    "dept_no" varchar(10)   NOT NULL,
+    "emp_no" int   NOT NULL,
+	 CONSTRAINT "pk_department_manager" PRIMARY KEY (
+        "emp_no")
+   
+     
+);
+
+CREATE TABLE "employees" (
+    "emp_no" int   NOT NULL,
+    "emp_title_id" varchar(10)   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar(50)   NOT NULL,
+    "last_name" varchar(50)   NOT NULL,
+    "sex" varchar(1)   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" dec   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "title" (
+    "title_id" varchar(10)   NOT NULL,
+    "title" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_title" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+--- After creating table Imported the data using import/export wizard ----
